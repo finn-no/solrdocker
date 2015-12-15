0d05efcc93c17188e90b99ee9ed06bdb486d83ed
chrkolst <christopher.kolstad@finn.no>
2015-12-15 15:27:09 +0100

    Tweak run-solr to expose jmx

--- run-solr.sh
+++ run-solr.sh
@@ -12,6 +12,8 @@ if [ -f /opt/run-java-options ]; then
   export CMD_OPTS="$CMD_OPTS -a $EXTRA_JAVA_OPTS"
 fi
 
+export JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=$RMI_PORT"
+
 cd /opt/solr
 
-./bin/solr $CMD_OPTS $SOLR_DATA_FOLDER
+./bin/solr $CMD_OPTS $SOLR_DATA_FOLDER $JMX_OPTS
